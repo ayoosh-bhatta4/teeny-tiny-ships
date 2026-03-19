334a7fbde61e899d72d898dc2fc4ae1997459001
Aayush Bhattacharya <161574215+ayoosh-bhatta4@users.noreply.github.com>
2026-03-19 22:18:27 +0000

    working ollama wrapper

---
+++ organizer/gpt_wrapper_script.py
@@ -0,0 +1,30 @@
+import requests
+usr_list = []
+while(True):
+    usr_in = input("Enter items of your list, type DONE to exit: ")
+    if(usr_in == "DONE"): break;
+    usr_list.append(usr_in)
+in_str = " ".join(usr_list)
+
+prompt = f'''INSTRUCTION: You are a system that will be given a list of items followed by their price. currency is rupees if not mentioned. your job is to replace the specific names of the items by their more generic category. For example, Uber beccomes transport. Swiggy becomes food and so on. If there are things which are in the same category, add up their prices. Only output the result. No explanantion. Output EXACTLY in this format: <category> <number> no extra words. 
+            EXAMPLE: if input is "swiggy 120 coffee 100 uber 300", you output
+                food 220
+                transport 300
+            INPUT: {in_str}
+            OUTPUT:'''
+
+
+
+response = requests.post(
+    "http://localhost:11434/api/generate",
+    json = {
+        "model" : "phi",
+        "prompt": prompt,
+        "stream": False
+    }
+)
+
+data = response.json()
+output = data["response"]
+
+print(output)
